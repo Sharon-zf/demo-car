2dccbcfb967a6d8a93d8f713725477235e016c6c
Sharon-zf <2335649528@qq.com>
2022-03-27 20:52:25 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="app-container">
+    <!-- 头部区域 -->
+    <Header title='购物车案例'></Header>
     <h1>App 根组件</h1>
   </div>
 </template>
 
 <script>
-export default {}
+import Header from '@/components/Header/Header.vue'
+import axios from 'axios'
+export default {
+  created(){
+    this.initCartList()
+  },
+  methods: {
+    async initCartList(){
+      const {data:res} = await axios.get('https://www.escook.cn/api/cart')
+      console.log(res)
+    }
+  },
+  components: {
+    Header
+  }
+}
 </script>
 
 <style lang="less" scoped>
